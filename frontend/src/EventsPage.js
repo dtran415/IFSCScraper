f591450f0ee03c8b2283f272524b626d86c4df1f
Denny <dtran415@gmail.com>
2023-08-10 10:21:25 -0700

    Switching events page to accordion

--- frontend/src/EventsPage.js
+++ frontend/src/EventsPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import IFSCScraperAPI from "./Api";
 import EventItem from "./EventItem";
+import { AccordionItem, AccordionHeader, AccordionBody, UncontrolledAccordion } from "reactstrap";
 
 function EventsPage() {
 
@@ -21,23 +22,79 @@ function EventsPage() {
         return "Loading...";
     }
 
-    const list = events.events.map(e => {
+    let accordionItems = [];
+    let eventsList = [];
+    let currentYear = null;
+    let firstYear = null;
+    // assume events are already ordered
+
+    for (let e of events.events) {
         const date = new Date(e.dateStart);
-        const options = {year: 'numeric', month: 'long', day: 'numeric'}
-        const event = {
-            id: e.id,
-            name: e.title,
-            date: date.toLocaleDateString("en-US", options),
-            subevents: e.SubEvents
+        const year = date.getFullYear();
+        // if we have events create accordion item
+        if (currentYear !== year) {
+            if (!firstYear)
+                firstYear = year;
+
+            if (eventsList.length !== 0) {
+                accordionItems.push(createAccordionItem(eventsList, currentYear));
+                // reset events list since it's been processed
+                eventsList = [];
+            }
+
+            currentYear = year;
         }
-        return <EventItem key={e.id} event={event} />
-    });
+
+        eventsList.push(e);
+    }
+
+    // get the rest
+    if (eventsList.length !== 0) {
+        accordionItems.push(createAccordionItem(eventsList, currentYear));
+    }
+
+    const list =
+        <UncontrolledAccordion defaultOpen={[firstYear.toString()]} stayOpen>
+            {accordionItems}
+        </UncontrolledAccordion>
 
     return (
-        <div>
+        <div className="container mt-5">
             {list}
         </div>
     )
 }
+
+function createEvent(eventInput) {
+    const date = new Date(eventInput.dateStart);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' }
+    const event = {
+        id: eventInput.id,
+        name: eventInput.title,
+        date: date.toLocaleDateString("en-US", options),
+        subevents: eventInput.SubEvents
+    }
+    const eventItem = <EventItem key={event.id} event={event} />
+    return eventItem;
+}
+
+function createAccordionItem(eventsList, currentYear) {
+    const accordionBody = [];
+    for (let e2 of eventsList) {
+        accordionBody.push(createEvent(e2));
+    }
+
+    const id = currentYear.toString();
+    const accordionItem =
+        <AccordionItem key={id}>
+            <AccordionHeader targetId={id}>{currentYear}</AccordionHeader>
+            <AccordionBody accordionId={id}>
+                {accordionBody}
+            </AccordionBody>
+        </AccordionItem>
+
+    return accordionItem;
+}
+
 
 export default EventsPage;
